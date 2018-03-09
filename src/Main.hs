892b968b173dbd692fe766d32b3e07435a4d2407
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-03-09 21:47:48 +1100

    task -> thread

--- src/Main.hs
+++ src/Main.hs
@@ -6,52 +6,52 @@ import Control.Monad.Loops
 import Control.Monad.Coroutine
 import Control.Monad.Coroutine.SuspensionFunctors
 
-data TaskStatus =
-    TaskPreempted | TaskSyscall
+data ThreadStatus =
+    ThreadPreempted | ThreadSyscall
         deriving Show
 
-type TaskState = Coroutine (Yield TaskStatus) IO String
+type ThreadState = Coroutine (Yield ThreadStatus) IO String
 
-task :: String -> TaskState
-task id = do
-    lift $ putStrLn ("Init task: " ++ id)
+loopyThread :: String -> ThreadState
+loopyThread id = do
+    lift $ putStrLn ("Init thread: " ++ id)
     whileM (return True) (do
-        lift $ putStrLn ("In task: " ++ id)
-        yield TaskPreempted)
-    lift $ putStrLn ("End of task: " ++ id)
+        lift $ putStrLn ("In thread: " ++ id)
+        yield ThreadPreempted)
+    lift $ putStrLn ("End of thread: " ++ id)
     return ("Return: " ++ id)
 
 -- Scheduler picks this to stop scheduling
-dieTask :: TaskState
-dieTask = do
+dieThread :: ThreadState
+dieThread = do
     lift $ putStrLn ("Dying...")
     return ("Returning dead")
 
-startWith = [task "A", task "B", task "cooltask"]
+startWith = [loopyThread "A", loopyThread "B"]
 
-data SchedulerState = SchedulerState { taskStates :: [TaskState]
+data SchedulerState = SchedulerState { threadStates :: [ThreadState]
                                      , lastPicked :: Int
                                      , pickIndex :: Int
                                      }
 
-schedulerDo :: Show a => SchedulerState -> Yield a TaskState -> (SchedulerState, TaskState)
-schedulerDo currentSchedulerState (Yield x newTaskState) =
-    ((newSchedulerState), lift (print x) >> pickTask)
+schedulerDo :: Show a => SchedulerState -> Yield a ThreadState -> (SchedulerState, ThreadState)
+schedulerDo currentSchedulerState (Yield x newThreadState) =
+    ((newSchedulerState), lift (print x) >> pickThread)
         where newSchedulerState :: SchedulerState
               newSchedulerState =
                 (SchedulerState
-                    ((taskStates currentSchedulerState) & element (lastPicked currentSchedulerState) .~ newTaskState)
+                    ((threadStates currentSchedulerState) & element (lastPicked currentSchedulerState) .~ newThreadState)
                     (newPickIndex)
                     (succ $ pickIndex currentSchedulerState))
               newPickIndex :: Int
-              newPickIndex = (pickIndex currentSchedulerState) `mod` (length $ taskStates currentSchedulerState)
-              pickTask :: TaskState
-              pickTask = if (pickIndex currentSchedulerState < 10)
-                            then (taskStates currentSchedulerState) !! newPickIndex
-                            else dieTask
+              newPickIndex = (pickIndex currentSchedulerState) `mod` (length $ threadStates currentSchedulerState)
+              pickThread :: ThreadState
+              pickThread = if (pickIndex currentSchedulerState < 10)
+                            then (threadStates currentSchedulerState) !! newPickIndex
+                            else dieThread
 
 
-runScheduler :: TaskState -> IO (SchedulerState, String)
+runScheduler :: ThreadState -> IO (SchedulerState, String)
 runScheduler = foldRun schedulerDo (SchedulerState startWith 0 1)
 
 main :: IO ()
